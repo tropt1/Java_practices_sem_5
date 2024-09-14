66cf2d5c070176cfcba43678d720478e21d5c15c
Bolotov Maxim <2717958@gmail.com>
2024-09-14 15:02:37 +0300

    Added first and second practices

---
+++ src/prac_1/ex_2.java
@@ -0,0 +1,29 @@
+package prac_1;
+import java.util.Scanner;
+
+
+public class ex_2 {
+    public static void main(String[] args){
+        final double RUBLES_PER_YUAN = 11.91;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Введите сумму в юанях: ");
+        int yuan = input.nextInt();
+
+        int digit = yuan % 10;
+        String word;
+
+        if (digit == 1 && yuan != 11)
+            word = " китайский юань";
+        else if (digit >= 2 && digit <= 4 && (yuan < 10 || yuan > 20))
+            word = " китайских юаня";
+        else
+            word = " китайских юаней";
+
+        double rubles = RUBLES_PER_YUAN * yuan;
+
+        System.out.println(yuan + word + " в рублях: " + rubles);
+    }
+
+}
---
+++ src/prac_2/ex_2.java
@@ -0,0 +1,28 @@
+package prac_2;
+
+public class ex_2 {
+    public static void main(String[] args) {
+        // тест конструкторов
+        Car car1 = new Car("Toyota", "ABC123", "Красная", 2015);
+        Car car2 = new Car();
+        Car car3 = new Car("Honda", 2018);
+        
+        // тест To_String()
+        System.out.println("Машина 1: " + car1.To_String());
+        System.out.println("Машина 2: " + car2.To_String());
+        System.out.println("Машина 3: " + car3.To_String());
+
+        // использование геттеров и сеттеров
+        car2.setModel("BMW");
+        car2.setLicense("ABC123");
+        car2.setColor("Черная");
+        car2.setYear(2020);
+
+        System.out.println("Обновленная машина 2: " + car2.To_String());
+
+        // получение возраста авто
+        final int CURRENT_YEAR = 2024;
+        System.out.println("Возраст машины 1: " + car1.getCarAge(CURRENT_YEAR));
+        System.out.println("Возраст машины 3: " + car3.getCarAge(CURRENT_YEAR));
+    }
+}
